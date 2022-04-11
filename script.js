e6b1d75bd363fc44bb6e0af8026a26c5c7b8ea01
ReiEmRo <97761942+ReiEmRo@users.noreply.github.com>
2022-04-10 21:53:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#navbar-area').load('navbar.js');
+
+    $.ajax({
+        url: "test_data/test_temp1.csv",
+        async: false,
+        dataType: "text",
+        success: function(data)
+        {
+            var temp_data = data.split(",");
+            console.log(temp_data);
+            for(var i = 0; i < temp_data.length; i++) {
+                $('#temp-data').append(temp_data[i] + " ");
+            }
+        }
+    })
+});
