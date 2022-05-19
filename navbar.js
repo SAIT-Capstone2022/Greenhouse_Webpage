c9355b08f6cfe731557d2605e79d67a3f0d4aa9a
Rei Rosell <reiemanuelr@gmail.com>
2022-05-19 13:31:04 -0600

    Changes to background colour.

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #545137">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Home</a>
+        <a class="navbar-brand" href="index.html">Green Peace</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
